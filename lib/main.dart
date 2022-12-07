97146540e68a7f175b7d8cc8ab0f05abd40879de
JuanPa505 <burguenojuanpa@gmail.com>
2022-12-06 22:01:30 -0700

    maquetacion lista y nodo single

--- lib/main.dart
+++ lib/main.dart
@@ -49,14 +49,45 @@ class _MyAppState extends State<MyApp> {
       title: 'Vende Todo App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Vende Todo App'),
+          title: const Text(
+            'Catalogo de Productos',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: const Color.fromARGB(255, 100, 126, 241),
+          elevation: 0.0,
         ),
         body: FutureBuilder(
           future: _listaProductos,
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return ListView(
-                children: _listProductos(snapshot.data),
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index].nombre),
+                    subtitle: Text(snapshot.data![index].descripcion),
+                    //snapshot.data![index].imagen,
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(10), // Image border
+                      child: SizedBox.fromSize(
+                        child: Image.network(snapshot.data![index].imagen,
+                            fit: BoxFit.cover, width: 80, height: 80),
+                      ),
+                    ),
+                    trailing: const Icon(Icons.arrow_forward_ios),
+                    contentPadding: const EdgeInsets.all(10),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DetalleProducto(
+                                  producto: snapshot.data![index])));
+                    },
+                  );
+                },
               );
             } else if (snapshot.hasError) {
               return const Text("Error");
@@ -67,22 +98,106 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+}
 
-  List<Widget> _listProductos(List<Producto>? data) {
-    List<Widget> productos = [];
+class DetalleProducto extends StatelessWidget {
+  final Producto producto;
 
-    for (var producto in data!) {
-      productos.add(Card(
-          child: Column(
-        children: [
-          Image.network(producto.imagen),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(producto.nombre),
+  const DetalleProducto({super.key, required this.producto});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(producto.nombre,
+            style: const TextStyle(
+                color: Colors.white,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.bold)),
+        backgroundColor: const Color.fromARGB(255, 100, 126, 241),
+      ),
+      body: Center(
+        child: Column(children: [
+          const Padding(padding: EdgeInsets.all(10)),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(25), // Image border
+            child: SizedBox.fromSize(
+              child: Image.network(producto.imagen,
+                  fit: BoxFit.cover, width: 380, height: 300),
+            ),
           ),
-        ],
-      )));
-    }
-    return productos;
+          const Padding(padding: EdgeInsets.all(15)),
+          Container(
+            width: 300,
+            height: 60,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 36, 98, 174),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text.rich(
+              TextSpan(
+                text: 'Producto: ',
+                style: const TextStyle(
+                    fontSize: 20, color: Colors.white), // default text style
+                children: <TextSpan>[
+                  TextSpan(
+                      text: producto.nombre,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20)),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(10)),
+          Container(
+            width: 300,
+            height: 60,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color.fromARGB(255, 36, 98, 174),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text.rich(
+              TextSpan(
+                text: 'Descripción: ',
+                style: const TextStyle(
+                    fontSize: 20, color: Colors.white), // default text style
+                children: <TextSpan>[
+                  TextSpan(
+                      text: producto.descripcion,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20)),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(10)),
+          Container(
+            width: 320,
+            height: 60,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 36, 98, 174),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Text.rich(
+              TextSpan(
+                text: 'Disponibilidad: ',
+                style: TextStyle(
+                    fontSize: 20, color: Colors.white), // default text style
+                children: <TextSpan>[
+                  TextSpan(
+                      text: "# de unidades",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                ],
+              ),
+            ),
+          ),
+          const Padding(padding: EdgeInsets.all(10)),
+        ]),
+      ),
+    );
   }
 }
